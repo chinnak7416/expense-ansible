c6c3c5fac219c3e2f7b04b02ccbbd9ec078239ef
RAMBABU <chinnak7416@gmail.com>
2024-03-16 21:31:47 +0530

    testing env variable

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
 component=$1
-ansible-playbook -i $component-dev.ramdevops78.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+env
+#ansible-playbook -i $component-dev.ramdevops78.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
 
