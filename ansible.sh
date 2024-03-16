a3341921903914f8f8dbca5420c4145817d050d6
RAMBABU <chinnak7416@gmail.com>
2024-03-16 17:06:00 +0530

    pipeline for frontend

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,3 @@
 component=$1
-ansible-playbook -i $component-dev.ramdevops78.online, -e ansible_username=ec2-user -e ansible_password=DevOps321 -e  env=dev -e role_name=$component expense.yml
+ansible-playbook -i $component-dev.ramdevops78.online, -e ansible_username=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+
