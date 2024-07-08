dabec5f115da304c806039711c9caab6c297e6f7
RAMBABU <chinnak7416@gmail.com>
2024-07-08 16:47:57 +0530

    Adding  vault encrypted token added to tf pipeline

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 component=$1
 
-#ansible-playbook -i $component-$env.ramdevops78.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.ramdevops78.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
 
 #when env=$2 declare then password should be enable & write every env=dev,qa,stage,prod in .yml scripts
