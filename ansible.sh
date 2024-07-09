ca9a33708b54427006e0b4079f8771cc24fa9d9d
RAMBABU <chinnak7416@gmail.com>
2024-07-09 23:47:15 +0530

    update code to get all secrets for ansible pull

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,7 @@ component=$1
 
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
+ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
+rm -rf @secrets.json @app.json
 
 #when env=$2 declare then password should be enable & write every env=dev,qa,stage,prod in .yml scripts
