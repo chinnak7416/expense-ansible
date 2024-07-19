b9bda6452d9a74ba2364c13c06d2886b0d90bece
RAMBABU <chinnak7416@gmail.com>
2024-07-19 11:51:14 +0530

    enabled rds db setup.

--- ansible.sh
+++ ansible.sh
@@ -1,8 +1,8 @@
 component=$1
 
-rm -rf @~/secrets.json @~/app.json
+rm -f  ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
-rm -rf @~/secrets.json @~/app.json
+ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+rm -f  ~/*.json
 
 #when env=$2 declare then password should be enable & write every env=dev,qa,stage,prod in .yml scripts
