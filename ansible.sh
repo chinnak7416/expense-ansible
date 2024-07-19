f41a6d2f9af046489dee7642e45052e617e9673c
RAMBABU <chinnak7416@gmail.com>
2024-07-19 22:43:58 +0530

    enabled lookup

--- ansible.sh
+++ ansible.sh
@@ -1,8 +1,8 @@
 component=$1
 
-rm -f  ~/*.json
+rm -rf  ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
-rm -f  ~/*.json
+rm -rf  ~/*.json
 
 #when env=$2 declare then password should be enable & write every env=dev,qa,stage,prod in .yml scripts
