5f9c460075f4de8931397bf075f3abfc66ce75f3
RAMBABU <chinnak7416@gmail.com>
2024-08-01 14:28:57 +0530

    Changed to ec2-describe instances in ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,10 @@ component=$1
 
 rm -rf  ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
-rm -rf  ~/*.json
+#ansible-playbook -i $component-$env.ramdevops78.online,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
 
+aws ec2 describe-instances --filters Name=tag:Name,Values=backend-dev Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+ansible-playbook -i inv,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json
+
+rm -rf  ~/*.json
 #when env=$2 declare then password should be enable & write every env=dev,qa,stage,prod in .yml scripts
